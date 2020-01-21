b94462f80c88deb3431bfa3849af6a7d0028da48
cedric-winbush <cawinbushjr@gmail.com>
2020-01-21 13:54:44 -0600

    make comments show

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,17 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [comments] = useState(props.comment);
+
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {props.comments.map(c=> (
+
+        <Comment comment={c} />
+      ))}
+
       <CommentInput />
     </div>
   );
